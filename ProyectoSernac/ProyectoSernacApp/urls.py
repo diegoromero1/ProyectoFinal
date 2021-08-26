589d9d2f07f2997c8963d461bd4333f829c4fb0f
diegoromero1 <70231529+diegoromero1@users.noreply.github.com>
2021-08-26 00:42:23 -0400

    Inicio

---
+++ ProyectoSernac/ProyectoSernacApp/urls.py
@@ -0,0 +1,24 @@
+from django.contrib import admin
+from django.urls import path, include
+from ProyectoSernacApp import views
+from django.conf import settings
+from django.conf.urls import url
+from django.contrib.auth import views as auth_views
+from django.conf.urls.static import static
+from django.contrib.auth.views import LoginView, LogoutView, PasswordResetView
+
+
+
+
+
+
+urlpatterns = [
+    path('',views.Inicio, name = "Inicio"),
+    path('login/', LoginView.as_view(template_name='ProyectoSernacApp/login.html'), name='login'),
+	path('logout/', LogoutView.as_view(template_name='ProyectoSernacApp/logout.html'), name='logout'),
+    path('Inicio',views.Inicio, name = "Inicio"),
+    path('accounts/profile/', views.Inicio, name = "Dentro"),   ### LUEGO DE INICIAR SESION MANDA A ACCOUNTS/PROFILE AJUSTÉ PARA IR A INICIO###
+    path('Registro',views.Registro, name = "Registro"),
+    url(r'^password/$', views.change_password, name='change_password'), ##CAMBIO PASSW
+    
+]
---
+++ ProyectoSernac/ProyectoSernacApp/views.py
@@ -0,0 +1,55 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.forms.widgets import CheckboxInput
+from django.shortcuts import render, HttpResponse, redirect
+from .forms import CustomUserForm
+from django.contrib.auth import login, authenticate
+from django.contrib import messages
+from django.contrib.auth import update_session_auth_hash, login, authenticate
+from django.contrib.auth.forms import PasswordChangeForm, PasswordResetForm
+from django.core.mail import send_mail, BadHeaderError
+from django.http import HttpResponse
+from django.contrib.auth.models import User
+
+
+
+#Vistas de la pagina
+def Login(request):
+    return render(request,"ProyectoSernacApp/Login.html")
+
+def Inicio(request):
+    return render(request,"ProyectoSernacApp/Inicio.html")
+
+def Registro(request):
+    data = {
+        'form': CustomUserForm()
+    }
+    if request.method == 'POST':
+        formulario = CustomUserForm(data=request.POST)
+        if formulario.is_valid():
+            formulario.save()
+            user = authenticate(username=formulario.cleaned_data["username"], password=formulario.cleaned_data["password1"])
+            login(request, user)
+            messages.success(request, "Te has registrado correctamente")
+            return redirect(to='Inicio')
+        data["form"] = formulario
+      
+    return render(request,"ProyectoSernacApp/Registro.html", data)
+
+
+## CAMBIO DE CONTRASEÑA ##
+def change_password(request):
+    if request.method == 'POST':
+        form = PasswordChangeForm(request.user, request.POST)
+        if form.is_valid():
+            user = form.save()
+            update_session_auth_hash(request, user)  # Important!
+            messages.success(request, 'Your password was successfully updated!')
+            return redirect('change_password')
+        else:
+            messages.error(request, 'Please correct the error below.')
+    else:
+        form = PasswordChangeForm(request.user)
+    return render(request, 'accounts/change_password.html', {
+        'form': form
+    })
+
